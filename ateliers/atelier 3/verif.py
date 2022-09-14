1351b91574c89aeef4480833dc337e5d2caefc3a
Antoine Marechal-Balle <59922574+Anto824@users.noreply.github.com>
2022-09-14 09:58:34 +0200

    .

--- ateliers/atelier 3/verif.py
+++ ateliers/atelier 3/verif.py
@@ -2,7 +2,7 @@ def ouvrante(car):
     return car == '(' or car == '[' or car == '{'
 
 def fermante(car):
-    return car == ')' or car == ']', or car == '}'
+    return car == ')' or car == ']' or car == '}'
 
 #def renverse(car):
 
