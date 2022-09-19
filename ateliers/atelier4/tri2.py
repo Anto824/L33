90306d973ab5fcfa5251f6e3bae7e6c168470739
Antoine Marechal-Balle <anto.marechal@icloud.com>
2022-09-19 12:40:53 +0200

    .

--- ateliers/atelier4/tri2.py
+++ ateliers/atelier4/tri2.py
@@ -33,3 +33,45 @@ def tri_insertion(T):
 
 #print(tri_insertion(L))
 
+
+
+def tri_selection(a):
+    for i in range(len(a)-1):
+        minimum= a[i:].index(min(a[i:]))
+        a[i], a[i+minimum] = a[i+minimum], a[i]
+    
+    return a
+
+def tri_bulle(a):
+    for i in range(len(a)):
+        for j in range(len(a)-1, i, -1):
+            if a[j] < a[j-1]:
+                a[j], a[j-1] = a[j-1], a[j]
+            
+    return a
+
+def fusion(l, r):
+    n = len(l + r) #Taille du tableau après la fusion
+    s = max(l + r) + 1 #Garde
+
+    l.append(s)
+    r.append(s)
+
+    a = []
+    while len(a) < n:
+        a.append(l.pop(0)) if l[0] <= r[0] else a.append(r.pop(0))
+    
+    return a
+
+def tri_fusion(a):
+    if len(a) == 1:
+        return a
+
+    mid = len(a) // 2
+    l = tri_fusion(a[:mid])
+    r = tri_fusion(a[mid:])
+    
+    return fusion(l, r)
+
+
+
